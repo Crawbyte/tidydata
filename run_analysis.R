0633ec3577eaeb202c9833585bf35f508e7a65ec
Crawbyte <cristian.roso@gmail.com>
2020-08-04 16:03:33 -0400

    Download the zip file in a new directory

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,15 @@
-
+# Library to use
+library(data.table)
+# Download the zip file in a new directory
+dataURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+temp <- tempfile()
+download.file(dataURL, temp)
+unzip(temp, 
+      files = NULL, 
+      list = FALSE, 
+      overwrite = TRUE, 
+      junkpaths = FALSE, 
+      exdir = ".", 
+      unzip = "internal", 
+      setTimes = FALSE)
+#
